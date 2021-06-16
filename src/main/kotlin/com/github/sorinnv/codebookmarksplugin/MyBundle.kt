7896030f8a2d2f2098b6b6a8c3abdf7a7db1c101
GitHub Action <action@github.com>
2021-06-16 16:44:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/sorinnv/codebookmarksplugin/MyBundle.kt
+++ src/main/kotlin/com/github/sorinnv/codebookmarksplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.sorinnv.codebookmarksplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/sorinnv/codebookmarksplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sorinnv.codebookmarksplugin.services
+
+import com.github.sorinnv.codebookmarksplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
